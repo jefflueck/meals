c9570a1cf6cdc171c4b4750e5ed082bebafd6385
jefflueck <lueckcfd@yahoo.com>
2024-10-17 13:10:56 -0500

    added side drawer to app, made custom widget, imported widget to be used in tabs.dart file

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -22,7 +22,7 @@ class MealDetailsScreen extends StatelessWidget {
             onPressed: () {
               onToggleFavorite(meal);
             },
-            icon: Icon(Icons.star),
+            icon: const Icon(Icons.star),
           ),
         ],
       ),
