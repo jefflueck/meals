2610f6c2499c562879ce026fb0854c22833c0241
jefflueck <lueckcfd@yahoo.com>
2024-10-21 20:34:24 -0500

    abstracted behavior to favorites_provider.dart, used StateNotifier in favorites_provider.dart to hold favorites state, used ConsumerState and ConsumerWidget in meals_details.dart and tabs.dart to manage get state, cleaned up meals.dart to initiate onpressed behavior and giving UI feedback.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,12 +7,11 @@ import 'package:meals/models/category.dart';
 import 'package:meals/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.onToggleFavorite,
-      required this.availableMeals});
+  const CategoriesScreen({
+    super.key,
+    required this.availableMeals,
+  });
 
-  final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -25,7 +24,6 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavorite: onToggleFavorite,
         ),
       ),
     ); // Navigator.push(context, route); alternative way to write this line of code.
